f98b37acae395f87f045a64727d396f75ee562ad
yuniel <yuniel2008@gmail.com>
2021-07-23 09:30:13 -0400

    [add] Agregado mecanismo para ver detalle en pruebas pcr

--- src/app/aplications/aplications-test/view-test/view-test.component.ts
+++ src/app/aplications/aplications-test/view-test/view-test.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Test} from '../test';
+import {TestService} from '../test.service';
 
 @Component({
   selector: 'app-view-test',
@@ -6,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-test.component.css']
 })
 export class ViewTestComponent implements OnInit {
+  public obj: Test = null;
+  public msgError = 'null';
+  public loading = false;
 
-  constructor() { }
+  constructor(
+    private service: TestService,
+    private router: Router,
+    private  route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params.id;
+    if (!id) { return; }
+
+    this.getByid(id);
+  }
+
+
+  getByid( id: string): void {
+    if (!id) {
+      id = 'vacio';
+    }
+
+    this.loading = false;
+    this.obj = null;
+
+    this.service.getByid(id)
+      .subscribe(
+        rt => {
+          if (rt.error) {
+            this.msgError = rt.error;
+          } else {
+            this.obj = rt.data;
+            this.loading = true;
+          }
+        },
+        er => {
+          this.msgError = er;
+          this.loading = true;
+
+        },
+        () => {
+
+          console.log('ready');
+        }
+      );
+  }
+
+  onClosed(): void {
+    this.msgError = 'null';
   }
 
 }
